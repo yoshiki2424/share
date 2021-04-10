f5d498176180b7dc8b8185c4a41522a133cca64b
yoshi <yoshiki24.2000@outlook.jp>
2021-04-10 23:20:50 +0900

    git commit -m 'first-commit'

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Profile from '../views/Profile.vue'
+import Detail from '../views/Detail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'sign',
+    component: SignUp
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: Detail,
+    props: true,
   }
 ]
 
---
+++ src/views/SignUp.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <HeaderAuth></HeaderAuth>
+    <div class = "card">
+      <p>新規登録</p>
+      <div class = "form">
+        <input type="text" placeholder="ユーザーネーム">
+        <input type="text" placeholder="プロフィール">
+        <input type="email" placeholder="メールアドレス">
+        <input type="password" placeholder="パスワード">
+        <button @click="auth">新規登録</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderAuth from "../components/HeaderAuth";
+export default{
+  components:{
+    HeaderAuth
+  }
+};
+</script>
+
+<style scoped>
+button{
+  width:100px;
+  text-align:center;
+  padding:8px 0 10px;
+  color:#fff;
+  background-color:#5419da;
+  border-radius:25px;
+  cursor:pointer;
+}
+
+.card{
+  margin:100px auto;
+  width:350px;
+  background-color:#fff;
+  border-radius:5px;
+  padding:20px;
+}
+
+.card p{
+  color:#000;
+  font-weight:bold;
+  text-align:center;
+}
+
+input{
+  margin-top:15px;
+  width:80%;
+  border-radius:10px;
+  padding:10px;
+  border:1px solid #000;
+  color:#000;
+}
+
+.form{
+  text-align:center;
+}
+
+.form button{
+  margin-top:15px;
+}
+</style>
